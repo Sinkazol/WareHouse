65084f4dd4a00f279c23f9d4926e4e96bca7fdfd
Sinkazol <81185967+Sinkazol@users.noreply.github.com>
2024-11-20 19:19:10 +0100

    add login and register controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context) : BaseApiController
+    {
+        [HttpPost("register")] //account/register
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                Name = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return user;
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user= await context.Users.FirstOrDefaultAsync(x=>
+            x.Name == loginDto.Username.ToLower());
+
+            if(user==null) return Unauthorized("Invalid username");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+            return user;
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x=>x.Name.ToLower() == username);
+        }
+    }
+}
